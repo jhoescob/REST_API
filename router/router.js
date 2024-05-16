ae9b5bb5550ce518fa9d7b50b861f1f129439b42
Jhoan Miguel Escobar <104960350+jhoescob@users.noreply.github.com>
2024-05-17 08:40:21 +0900

    fix the local json read with require import 2

--- router/router.js
+++ router/router.js
@@ -1,17 +1,12 @@
 import { Router } from "express";
-import data from "../data.json" with { type: "json" };
-import { validateMovie, validatePartialMovie } from "../schemas-zod/movies.js";
 
-import { MovieController } from '../controller/controller.js'
-
+import { MovieController } from "../controller/controller.js";
 
 export const moviesRouter = Router();
 
 moviesRouter.get("/", MovieController.getAll);
 
-
 moviesRouter.get("/:id", MovieController.getById);
-
 
 moviesRouter.post("/", MovieController.create);
 
